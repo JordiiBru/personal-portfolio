7d0cd57485ad3881c5539159b633a0246962b9e3
jordiibru <jordibrucarci@gmail.com>
2023-08-24 21:55:32 +0200

    add(): some fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,11 +6,11 @@ export default function About() {
         <h1 className={styles.title}>About Me</h1>
 
         <p className={styles.description}>
-          Hi! My name's Jordi, a Computer Engineering student at UPC-FIB and current <a href="https://en.wikipedia.org/wiki/Site_reliability_engineering" className={styles.link}>Site Reliability Engineer</a> intern based in Barcelona.
+          Hi! My name's Jordi, a Computer Engineering student at UPC-FIB and current <a href="https://en.wikipedia.org/wiki/Site_reliability_engineering" className={styles.link}>Site Reliability Engineer</a> intern.
           <br></br><br></br>
           With a keen interest in <a href="https://en.wikipedia.org/wiki/DevOps" className={styles.link}>DevOps</a> practices, my current focus is on learning about the reliability of services and their infrastructure, specializing in cloud (AWS), Terraform, and Kubernetes.
           <br></br><br></br>
-          I relentlessly pursue what I am most passionate about in this sector, while dedicating myself to learning and creating efficient solutions. This is just the beggining!
+          I relentlessly pursue what I am most passionate about in this sector, while dedicating myself to learning and creating efficient solutions. And this is just the beggining :)
           <br></br><br></br>
           You can reach out to me on <a href="https://www.linkedin.com/in/jordi-bru-carci-65b9b7276/" className={styles.link}>LinkedIn</a>.
         </p>
